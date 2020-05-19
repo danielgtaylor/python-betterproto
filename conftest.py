de61ddab21a9e3e34a4ffa7fd3df7fa64a594b85
James Lan <jameslan@gmail.com>
2020-05-19 10:26:23 -0700

    Add option to repeatly execute betterproto operations in test, to evaluate performance

---
+++ conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--repeat", type=int, default=1, help="repeat the operation multiple times")
+
+
+@pytest.fixture(scope="session")
+def repeat(request):
+    return request.config.getoption("repeat")
