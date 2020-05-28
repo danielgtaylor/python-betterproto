91111ab7d8e6b4c3d510b7c044c58f915f5db177
nat <n@natn.me>
2020-05-28 19:28:53 +0200

    Make plugin import errors more helpful
    This addresses an issue where if the user happens to have black installed in
    their environment but not the other dependencies when running the protoc
    plugin then the resulting import error (No module named 'google') is not very
    helpful.

--- betterproto/tests/inputs/config.py
+++ betterproto/tests/inputs/config.py
@@ -11,7 +11,7 @@ tests = {
     "googletypes_service_returns_empty",  # 9
     "casing_message_field_uppercase",  # 11
     "namespace_keywords",  # 70
-    "namespace_builtin_types"  # 53
+    "namespace_builtin_types",  # 53
 }
 
 services = {
