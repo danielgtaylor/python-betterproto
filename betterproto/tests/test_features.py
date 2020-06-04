a914306f33b47e56f4f0a51d45c9da1f06b48e88
Danny Weinberg <FuegoFro@gmail.com>
2020-06-04 13:42:07 -0700

    Put test into `test_features`, simplify to call `parse` directly

--- betterproto/tests/test_features.py
+++ betterproto/tests/test_features.py
@@ -1,6 +1,6 @@
 import betterproto
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List, Dict
 
 
 def test_has_field():
@@ -31,6 +31,21 @@ def test_has_field():
     # Can manually set it but defaults to false
     foo.bar = Bar()
     assert betterproto.serialized_on_wire(foo.bar) == False
+
+    @dataclass
+    class WithCollections(betterproto.Message):
+        test_list: List[str] = betterproto.string_field(1)
+        test_map: Dict[str, str] = betterproto.map_field(2, betterproto.TYPE_STRING, betterproto.TYPE_STRING)
+
+    # Unset with empty collections
+    with_collections_empty = WithCollections().parse(bytes(WithCollections()))
+    assert betterproto.serialized_on_wire(with_collections_empty) == False
+
+    # Set with non-empty collections
+    with_collections_list = WithCollections().parse(bytes(WithCollections(test_list=['a', 'b', 'c'])))
+    assert betterproto.serialized_on_wire(with_collections_list) == True
+    with_collections_map = WithCollections().parse(bytes(WithCollections(test_map={'a': 'b', 'c': 'd'})))
+    assert betterproto.serialized_on_wire(with_collections_map) == True
 
 
 def test_class_init():
