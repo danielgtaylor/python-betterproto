67422db6b928079f0248e22afe7a9229a567e95f
Danny Weinberg <FuegoFro@gmail.com>
2020-06-04 11:34:20 -0700

    Fix formatting

--- betterproto/tests/inputs/repeated/test_repeated.py
+++ betterproto/tests/inputs/repeated/test_repeated.py
@@ -13,19 +13,14 @@ from betterproto.tests.output_betterproto.repeated.repeated import (
 
 
 class ExampleService:
-    async def DoThing(
-        self, stream: "grpclib.server.Stream[Test, Test]"
-    ):
+    async def DoThing(self, stream: "grpclib.server.Stream[Test, Test]"):
         request = await stream.recv_message()
         await stream.send_message(request)
 
     def __mapping__(self) -> Dict[str, grpclib.const.Handler]:
         return {
             "/repeated.ExampleService/DoThing": grpclib.const.Handler(
-                self.DoThing,
-                grpclib.const.Cardinality.UNARY_UNARY,
-                Test,
-                Test,
+                self.DoThing, grpclib.const.Cardinality.UNARY_UNARY, Test, Test,
             ),
         }
 
@@ -34,6 +29,6 @@ class ExampleService:
 async def test_sets_serialized_on_wire() -> None:
     async with ChannelFor([ExampleService()]) as channel:
         stub = ExampleServiceStub(channel)
-        response = await stub.do_thing(names=['a', 'b', 'c'])
+        response = await stub.do_thing(names=["a", "b", "c"])
         assert betterproto.serialized_on_wire(response)
-        assert response.names == ['a', 'b', 'c']
+        assert response.names == ["a", "b", "c"]
