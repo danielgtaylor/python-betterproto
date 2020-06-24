bbf40f969469f4a70ffa8c1ce800ddaf49bdb8a7
Bouke Versteegh <boukeversteegh@users.noreply.github.com>
2020-06-24 21:17:59 +0200

    Mark test xfail

--- betterproto/tests/inputs/to_dict_with_missing_enum/test_to_dict_with_missing_enum.py
+++ betterproto/tests/inputs/to_dict_with_missing_enum/test_to_dict_with_missing_enum.py
@@ -2,15 +2,17 @@ from betterproto.tests.output_betterproto.to_dict_with_missing_enum.to_dict_with
     Test,
     TestMyEnum,
 )
+import pytest
 
 
+@pytest.mark.xfail(reason="#93")
 def test_message_attributes():
     assert (
-        Test(x=TestMyEnum.ONE).to_dict()['x'] == "ONE"
+        Test(x=TestMyEnum.ONE).to_dict()["x"] == "ONE"
     ), "MyEnum.ONE is not serialized to 'ONE'"
     assert (
-        Test(x=TestMyEnum.THREE).to_dict()['x'] == "THREE"
+        Test(x=TestMyEnum.THREE).to_dict()["x"] == "THREE"
     ), "MyEnum.THREE is not serialized to 'THREE'"
     assert (
-        Test(x=TestMyEnum.FOUR).to_dict()['x'] == "FOUR"
+        Test(x=TestMyEnum.FOUR).to_dict()["x"] == "FOUR"
     ), "MyEnum.FOUR is not serialized to 'FOUR'"
