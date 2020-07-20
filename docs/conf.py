c19ba2e802ab210eee76c2773f5bf2293017a754
James <gobot1234yt@gmail.com>
2020-07-20 13:45:57 +0100

    Another theme

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ pygments_style = "friendly"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'mxtheme'
+html_theme = 'sphinx_iowmd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
