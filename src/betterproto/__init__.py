a93bf39e81dd69b1e8285600a31a22ec58039954
James <50501825+Gobot1234@users.noreply.github.com>
2020-08-17 13:29:02 +0100

    Respond to review

--- src/betterproto/__init__.py
+++ src/betterproto/__init__.py
@@ -818,10 +818,9 @@ class Message(ABC):
         """
         output: Dict[str, Any] = {}
         field_types = self._type_hints()
-        proto_meta = self._betterproto
-        for field_name, meta in proto_meta.meta_by_field_name.items():
+        for field_name, meta in self._betterproto.meta_by_field_name.items():
             field_type = field_types[field_name]
-            field_is_repeated = proto_meta.default_gen[field_name] is list
+            field_is_repeated = self._betterproto.default_gen[field_name] is list
             value = getattr(self, field_name)
             cased_name = casing(field_name).rstrip("_")  # type: ignore
             if meta.proto_type == TYPE_MESSAGE:
