7746b91aa267db15c0e2fa2ba5f02ffd4cd730f3
James <gobot1234yt@gmail.com>
2020-08-24 14:10:28 +0100

    Add a test for it

--- tests/test_features.py
+++ tests/test_features.py
@@ -317,3 +317,26 @@ def test_oneof_default_value_set_causes_writes_wire():
         == betterproto.which_one_of(_round_trip_serialization(foo3), "group1")
         == ("", None)
     )
+
+
+def test_bool():
+    # evaluates similarly to a collection
+    # >>> bool(list())
+    # False
+    # >>> bool([1]]
+    # True
+    # >>> bool([0])
+    # True
+
+    @dataclass
+    class Falsy(betterproto.Message):
+        pass
+
+    @dataclass
+    class Truthy(betterproto.Message):
+        bar: int = betterproto.int32_field(1)
+
+    assert not Falsy()
+    assert not Truthy()
+    assert Truthy(bar=1)
+    assert Truthy(bar=0)
