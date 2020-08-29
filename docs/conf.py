3c08cb8f2bf1cd53fdb17a17320b2690538e4ad4
James <gobot1234yt@gmail.com>
2020-08-29 20:22:36 +0100

    Remove unused sections

--- docs/conf.py
+++ docs/conf.py
@@ -48,9 +48,6 @@ intersphinx_mapping = {
     "py": ("https://docs.python.org/3", None),
 }
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -61,8 +58,3 @@ pygments_style = "friendly"
 # a list of builtin themes.
 
 html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
