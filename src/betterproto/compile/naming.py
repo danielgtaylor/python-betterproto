9b9f737d1a6ebce0872e4432bcc25b09d3b0160e
James <gobot1234yt@gmail.com>
2020-12-30 21:18:20 +0000

    Remove any repeated prefix from enum members

--- src/betterproto/compile/naming.py
+++ src/betterproto/compile/naming.py
@@ -1,3 +1,7 @@
+import re
+from typing import Iterable, List
+
+import itertools
 from betterproto import casing
 
 
@@ -13,9 +17,17 @@ def pythonize_method_name(name: str) -> str:
     return casing.safe_snake_case(name)
 
 
-def pythonize_enum_member_name(name: str, enum_name: str) -> str:
-    enum_name = casing.snake_case(enum_name).upper()
-    find = name.find(enum_name)
-    if find != -1:
-        name = name[find + len(enum_name) :].strip("_")
-    return casing.sanitize_name(name)
+def pythonize_enum_member_names(names: Iterable[str]) -> List[str]:
+    """Removes any of the same characters from an Enum member's names."""
+
+    def find_max_prefix_len() -> int:
+        for max_prefix_len, chars in enumerate(tuple(zip(*map(tuple, names)))):
+            previous_char = chars[0][0]
+            for char in chars:
+                if previous_char != char:
+                    return max_prefix_len
+        return 0
+
+    max_prefix_len = find_max_prefix_len()
+
+    return [casing.sanitize_name(name[max_prefix_len:]) for name in names]
