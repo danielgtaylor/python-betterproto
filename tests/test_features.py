82c08357958e6e9e480c8c7d0c21d2aabdfbb171
crflynn <flynn@simplebet.io>
2021-01-28 21:16:51 -0500

    add to/from_pydict methods

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import betterproto
 from dataclasses import dataclass
 from typing import Optional, List, Dict
@@ -65,6 +67,7 @@ def test_class_init():
     foo = Foo(name="foo", child=Bar(name="bar"))
 
     assert foo.to_dict() == {"name": "foo", "child": {"name": "bar"}}
+    assert foo.to_pydict() == {"name": "foo", "child": {"name": "bar"}}
 
 
 def test_enum_as_int_json():
@@ -83,6 +86,11 @@ def test_enum_as_int_json():
     # Plain-ol'-ints should serialize properly too.
     foo.bar = 1
     assert foo.to_dict() == {"bar": "ONE"}
+
+    # Similar expectations for pydict
+    foo = Foo().from_pydict({"bar": 1})
+    assert foo.bar == TestEnum.ONE
+    assert foo.to_pydict() == {"bar": TestEnum.ONE}
 
 
 def test_unknown_fields():
@@ -174,8 +182,20 @@ def test_json_casing():
         "snakeCase": 3,
         "kabobCase": 4,
     }
+    assert test.to_pydict() == {
+        "pascalCase": 1,
+        "camelCase": 2,
+        "snakeCase": 3,
+        "kabobCase": 4,
+    }
 
     assert test.to_dict(casing=betterproto.Casing.SNAKE) == {
+        "pascal_case": 1,
+        "camel_case": 2,
+        "snake_case": 3,
+        "kabob_case": 4,
+    }
+    assert test.to_pydict(casing=betterproto.Casing.SNAKE) == {
         "pascal_case": 1,
         "camel_case": 2,
         "snake_case": 3,
@@ -216,12 +236,32 @@ def test_to_dict_default_values():
         "someBool": False,
     }
 
+    test = TestMessage().from_pydict({})
+
+    assert test.to_pydict(include_default_values=True) == {
+        "someInt": 0,
+        "someDouble": 0.0,
+        "someStr": "",
+        "someBool": False,
+    }
+
     # All default values
     test = TestMessage().from_dict(
         {"someInt": 0, "someDouble": 0.0, "someStr": "", "someBool": False}
     )
 
     assert test.to_dict(include_default_values=True) == {
+        "someInt": 0,
+        "someDouble": 0.0,
+        "someStr": "",
+        "someBool": False,
+    }
+
+    test = TestMessage().from_pydict(
+        {"someInt": 0, "someDouble": 0.0, "someStr": "", "someBool": False}
+    )
+
+    assert test.to_pydict(include_default_values=True) == {
         "someInt": 0,
         "someDouble": 0.0,
         "someStr": "",
@@ -264,6 +304,30 @@ def test_to_dict_default_values():
         "someDefaultBool": False,
     }
 
+    test = TestMessage2().from_pydict(
+        {
+            "someInt": 2,
+            "someDouble": 1.2,
+            "someStr": "hello",
+            "someBool": True,
+            "someDefaultInt": 0,
+            "someDefaultDouble": 0.0,
+            "someDefaultStr": "",
+            "someDefaultBool": False,
+        }
+    )
+
+    assert test.to_pydict(include_default_values=True) == {
+        "someInt": 2,
+        "someDouble": 1.2,
+        "someStr": "hello",
+        "someBool": True,
+        "someDefaultInt": 0,
+        "someDefaultDouble": 0.0,
+        "someDefaultStr": "",
+        "someDefaultBool": False,
+    }
+
     # Nested messages
     @dataclass
     class TestChildMessage(betterproto.Message):
@@ -281,6 +345,36 @@ def test_to_dict_default_values():
         "someInt": 0,
         "someDouble": 1.2,
         "someMessage": {"someOtherInt": 0},
+    }
+
+    test = TestParentMessage().from_pydict({"someInt": 0, "someDouble": 1.2})
+
+    assert test.to_pydict(include_default_values=True) == {
+        "someInt": 0,
+        "someDouble": 1.2,
+        "someMessage": {"someOtherInt": 0},
+    }
+
+
+def test_to_dict_datetime_values():
+    @dataclass
+    class TestDatetimeMessage(betterproto.Message):
+        bar: datetime = betterproto.message_field(1)
+        baz: timedelta = betterproto.message_field(2)
+
+    test = TestDatetimeMessage().from_dict(
+        {"bar": "2020-01-01T00:00:00Z", "baz": "86400.000s"}
+    )
+
+    assert test.to_dict() == {"bar": "2020-01-01T00:00:00Z", "baz": "86400.000s"}
+
+    test = TestDatetimeMessage().from_pydict(
+        {"bar": datetime(year=2020, month=1, day=1), "baz": timedelta(days=1)}
+    )
+
+    assert test.to_pydict() == {
+        "bar": datetime(year=2020, month=1, day=1),
+        "baz": timedelta(days=1),
     }
 
 
