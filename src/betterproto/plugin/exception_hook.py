e595356a753e351621dfe6aca1a5b7ffd5bb53f2
James Hilton-Balfe <50501825+Gobot1234@users.noreply.github.com>
2022-02-07 18:30:22 +0000

    Revive this

---
+++ src/betterproto/plugin/exception_hook.py
@@ -0,0 +1,50 @@
+import sys
+import traceback
+from pathlib import Path
+from types import TracebackType
+from typing import Type
+
+IMPORT_ERROR_MESSAGE = (
+    "Unable to import `{0.name}` from betterproto plugin! Please ensure that you've "
+    'installed betterproto as `pip install "betterproto[compiler]"` so that compiler '
+    "dependencies are included."
+)
+
+STDLIB_MODULES = getattr(
+    sys,
+    "builtin_module_names",
+    [
+        p.stem
+        for p in Path(traceback.__file__).parent.iterdir()
+        if p.suffix == ".py" or p.is_dir()
+    ],
+)
+
+
+def import_exception_hook(
+    type: Type[BaseException], value: ImportError, tb: TracebackType
+) -> None:
+    """Set an exception hook to automatically print:
+
+    "Unable to import `x` from betterproto plugin! Please ensure that you've installed
+    betterproto as `pip install "betterproto[compiler]"` so that compiler dependencies are
+    included."
+
+    if the module imported is not found and the exception is raised in this sub module
+    """
+    bottom_frame = list(traceback.walk_tb(tb))[-1][0]
+    module = bottom_frame.f_globals.get("__name__", "__main__")
+    if (
+        not module.startswith(__name__)
+        or not isinstance(value, ImportError)
+        or value.name in STDLIB_MODULES
+        or (value.name or "").startswith("betterproto")
+    ):
+        return sys.__excepthook__(type, value, tb)
+
+    print(f"\033[31m{IMPORT_ERROR_MESSAGE.format(value)}\033[0m", file=sys.stderr)
+    exit(1)
+
+
+def install_exception_hook():
+    sys.excepthook = import_exception_hook
