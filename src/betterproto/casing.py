a2ba23ef5256461e228f92e1a4a6458506588f07
James Hilton-Balfe <gobot1234yt@gmail.com>
2022-04-21 15:42:51 +0100

    Update casing.py

--- src/betterproto/casing.py
+++ src/betterproto/casing.py
@@ -138,6 +138,6 @@ def sanitize_name(value: str) -> str:
     # https://www.python.org/dev/peps/pep-0008/#descriptive-naming-styles
     if keyword.iskeyword(value):
         return f"{value}_"
-    if value.isidentifier():
+    if not value.isidentifier():
         return f"_{value}"
     return value
