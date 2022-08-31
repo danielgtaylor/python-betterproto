6c90b73f71cc8b24d38bc67fe91f644bf085906d
STerliakov <terlya.stas@gmail.com>
2022-08-31 20:16:27 +0300

    Add regression test for #387

---
+++ tests/inputs/regression_387/test_regression_387.py
@@ -0,0 +1,9 @@
+from tests.output_betterproto.regression_387 import Test, ParentElement
+
+
+def test_regression_387():
+    el = ParentElement(name="test", elems=[Test(id=0), Test(id=42)])
+    binary = bytes(el)
+    decoded = ParentElement().parse(binary)
+    assert decoded == el
+    assert decoded.elems == [Test(id=0), Test(id=42)]
