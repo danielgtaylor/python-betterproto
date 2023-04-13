6857f44af148d1b60ef23b5e9c9017b5160db361
James Hilton-Balfe <gobot1234yt@gmail.com>
2023-04-13 22:57:02 +0100

    One day I'll learn to read the diff

--- tests/inputs/timestamp_dict_encode/test_timestamp_dict_encode.py
+++ tests/inputs/timestamp_dict_encode/test_timestamp_dict_encode.py
@@ -1,6 +1,8 @@
-from datetime import datetime
-from datetime import timedelta
-from datetime import timezone
+from datetime import (
+    datetime,
+    timedelta,
+    timezone,
+)
 
 import pytest
 
