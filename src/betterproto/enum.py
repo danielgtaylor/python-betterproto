0582bb96f7e73d8e1810da9fb943f4103e8f73e4
Gobot1234 <gobot1234yt@gmail.com>
2023-08-17 17:59:33 +0100

    Fix a couple of things

--- src/betterproto/enum.py
+++ src/betterproto/enum.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import sys
 from enum import (
     EnumMeta,
@@ -8,10 +10,8 @@ from typing import (
     TYPE_CHECKING,
     Any,
     Dict,
-    NoReturn,
     Optional,
     Tuple,
-    TypeVar,
 )
 
 
@@ -21,7 +21,10 @@ if TYPE_CHECKING:
         Mapping,
     )
 
-    from typing_extensions import Self
+    from typing_extensions import (
+        Never,
+        Self,
+    )
 
 
 def _is_descriptor(obj: object) -> bool:
@@ -31,12 +34,12 @@ def _is_descriptor(obj: object) -> bool:
 
 
 class EnumType(EnumMeta if TYPE_CHECKING else type):
-    _value_map_: "Mapping[int, Enum]"
-    _member_map_: "Mapping[str, Enum]"
+    _value_map_: Mapping[int, Enum]
+    _member_map_: Mapping[str, Enum]
 
     def __new__(
         mcs, name: str, bases: Tuple[type, ...], namespace: Dict[str, Any]
-    ) -> "Self":
+    ) -> Self:
         value_map = {}
         member_map = {}
 
@@ -72,7 +75,7 @@ class EnumType(EnumMeta if TYPE_CHECKING else type):
 
             member = value_map.get(value)
             if member is None:
-                member = cls.__new__(cls, name=name, value=value)
+                member = cls.__new__(cls, name=name, value=value)  # type: ignore
                 value_map[value] = member
             member_map[name] = member
             type.__setattr__(new_mcs, name, member)
@@ -81,30 +84,30 @@ class EnumType(EnumMeta if TYPE_CHECKING else type):
 
     if not TYPE_CHECKING:
 
-        def __call__(cls, value: int) -> "Enum":
+        def __call__(cls, value: int) -> Enum:
             try:
                 return cls._value_map_[value]
             except (KeyError, TypeError):
                 raise ValueError(f"{value!r} is not a valid {cls.__name__}") from None
 
-        def __iter__(cls) -> "Generator[Enum, None, None]":
+        def __iter__(cls) -> Generator[Enum, None, None]:
             yield from cls._member_map_.values()
 
         if sys.version_info >= (3, 8):  # 3.8 added __reversed__ to dict_values
 
-            def __reversed__(cls) -> "Generator[Enum, None, None]":
+            def __reversed__(cls) -> Generator[Enum, None, None]:
                 yield from reversed(cls._member_map_.values())
 
         else:
 
-            def __reversed__(cls) -> "Generator[Enum, None, None]":
+            def __reversed__(cls) -> Generator[Enum, None, None]:
                 yield from reversed(tuple(cls._member_map_.values()))
 
-        def __getitem__(cls, key: str) -> "Enum":
+        def __getitem__(cls, key: str) -> Enum:
             return cls._member_map_[key]
 
         @property
-        def __members__(cls) -> "MappingProxyType[str, Enum]":
+        def __members__(cls) -> MappingProxyType[str, Enum]:
             return MappingProxyType(cls._member_map_)
 
     def __repr__(cls) -> str:
@@ -113,10 +116,10 @@ class EnumType(EnumMeta if TYPE_CHECKING else type):
     def __len__(cls) -> int:
         return len(cls._member_map_)
 
-    def __setattr__(cls, name: str, value: Any) -> NoReturn:
+    def __setattr__(cls, name: str, value: Any) -> Never:
         raise AttributeError(f"{cls.__name__}: cannot reassign Enum members.")
 
-    def __delattr__(cls, name: str) -> NoReturn:
+    def __delattr__(cls, name: str) -> Never:
         raise AttributeError(f"{cls.__name__}: cannot delete Enum members.")
 
     def __contains__(cls, member: object) -> bool:
@@ -132,11 +135,13 @@ class Enum(IntEnum if TYPE_CHECKING else int, metaclass=EnumType):
     name: Optional[str]
     value: int
 
-    def __new__(cls, *, name: Optional[str], value: int) -> "Self":
-        self = super().__new__(cls, value)
-        super().__setattr__(self, "name", name)
-        super().__setattr__(self, "value", value)
-        return self
+    if not TYPE_CHECKING:
+
+        def __new__(cls, *, name: Optional[str], value: int) -> Self:
+            self = super().__new__(cls, value)
+            super().__setattr__(self, "name", name)
+            super().__setattr__(self, "value", value)
+            return self
 
     def __str__(self) -> str:
         return self.name or "None"
@@ -144,18 +149,18 @@ class Enum(IntEnum if TYPE_CHECKING else int, metaclass=EnumType):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}.{self.name}"
 
-    def __setattr__(self, key: str, value: Any) -> NoReturn:
+    def __setattr__(self, key: str, value: Any) -> Never:
         raise AttributeError(
             f"{self.__class__.__name__} Cannot reassign a member's attributes."
         )
 
-    def __delattr__(self, item: Any) -> NoReturn:
+    def __delattr__(self, item: Any) -> Never:
         raise AttributeError(
             f"{self.__class__.__name__} Cannot delete a member's attributes."
         )
 
     @classmethod
-    def try_value(cls, value: int = 0) -> "Self":
+    def try_value(cls, value: int = 0) -> Self:
         """Return the value which corresponds to the value.
 
         Parameters
@@ -175,7 +180,7 @@ class Enum(IntEnum if TYPE_CHECKING else int, metaclass=EnumType):
             return cls.__new__(cls, name=None, value=value)
 
     @classmethod
-    def from_string(cls, name: str) -> "Self":
+    def from_string(cls, name: str) -> Self:
         """Return the value which corresponds to the string name.
 
         Parameters
