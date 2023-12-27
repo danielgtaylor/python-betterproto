c8e6244098a1e7b45d4cb81acbf0c51941d60cf5
William Woodruff <william@trailofbits.com>
2023-12-27 17:16:07 -0500

    tests: add Struct roundtrip tests
    Signed-off-by: William Woodruff <william@trailofbits.com>

---
+++ tests/test_struct.py
@@ -0,0 +1,22 @@
+import json
+
+from betterproto.lib.google.protobuf import Struct
+
+
+def test_struct_roundtrip():
+    data = {
+        "foo": "bar",
+        "baz": None,
+        "quux": 123,
+        "zap": [1, {"two": 3}, "four"],
+    }
+    data_json = json.dumps(data)
+
+    struct_from_dict = Struct().from_dict(data)
+    assert struct_from_dict.fields == data
+    assert struct_from_dict.to_json() == data_json
+
+    struct_from_json = Struct().from_json(data_json)
+    assert struct_from_json.fields == data
+    assert struct_from_json == struct_from_dict
+    assert struct_from_json.to_json() == data_json
