48f2c891dadfe05e296c8407189edda9a40d2958
Veaceslav Dimitrioglo <veaceslav.dimitrioglo-contractor@robertwalters.com>
2024-02-23 10:25:06 +0200

    style: run pre-commit hooks

--- tests/inputs/service_complex_structure/test_service_complex_structure.py
+++ tests/inputs/service_complex_structure/test_service_complex_structure.py
@@ -3,15 +3,23 @@ import pytest
 
 def test_payloads_import_is_generated():
     try:
-        from tests.output_betterproto.service_complex_structure.service import _payloads__
-        assert hasattr(_payloads__, 'Test')
+        from tests.output_betterproto.service_complex_structure.service import (
+            _payloads__,
+        )
+
+        assert hasattr(_payloads__, "Test")
     except ImportError:
         pytest.fail("Required import for type annotations is not generated!")
 
 
 def test_payloads_import_is_generated_pydantic():
     try:
-        from tests.output_betterproto_pydantic.service_complex_structure.service import _payloads__
-        assert hasattr(_payloads__, 'Test')
+        from tests.output_betterproto_pydantic.service_complex_structure.service import (
+            _payloads__,
+        )
+
+        assert hasattr(_payloads__, "Test")
     except ImportError:
-        pytest.fail("Required import for type annotations is not generated in pydantic output!")
+        pytest.fail(
+            "Required import for type annotations is not generated in pydantic output!"
+        )
