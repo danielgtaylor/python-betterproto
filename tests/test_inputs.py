a3d55cc04d3aef06d7dbc57d17312158134d2ec0
Adrien Vannson <adrien.vannson@protonmail.com>
2024-11-12 11:46:44 +0100

    Fix snake_case test

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -185,7 +185,12 @@ def test_message_json(repeat, test_data: TestData) -> None:
             message: betterproto.Message = plugin_module.Test()
 
             message.from_json(sample.json)
-            message_json = message.to_json(0)
+            message_json = message.to_json(indent=0)
+
+
+            print(message)
+            print(message_json)
+            print(message.to_dict())
 
             assert dict_replace_nans(json.loads(message_json)) == dict_replace_nans(
                 json.loads(sample.json)
