36b5fd1495325b0ad54701e583fb77efa921613b
Adrien <adrien.vannson@protonmail.com>
2025-01-03 03:01:36 +0100

    Don't repeat tests (#643)
    * Don't repeat tests
    
    * Format code

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,6 @@ import sys
 import pytest
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--repeat", type=int, default=1, help="repeat the operation multiple times"
-    )
-
-
-@pytest.fixture(scope="session")
-def repeat(request):
-    return request.config.getoption("repeat")
-
-
 @pytest.fixture
 def reset_sys_path():
     original = copy.deepcopy(sys.path)
